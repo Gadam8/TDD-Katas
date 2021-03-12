2c1b228d4639cee6e14f8db137f3a95c290a5ccc
Gadam8 <44494964+Gadam8@users.noreply.github.com>
2021-03-12 12:05:39 +0000

    Update HarryPoter.cs

--- src/TheHarryPoterKata/HarryPoter.cs
+++ src/TheHarryPoterKata/HarryPoter.cs
@@ -1,8 +1,14 @@
-﻿// Copyright (c) Gaurav Aroraa
+// Copyright (c) Gaurav Aroraa
 // Licensed under the MIT License. See License.txt in the project root for license information.
-namespace TDD_Katas_project.TheHarryPoterKata
+using System;
+
+namespace TDD_Katas_project.TheHarryPotterKata
 {
-    public class HarryPoter
+    public class HarryPotter
     {
+        public double CalculatePrice(int[] bookTitles)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
